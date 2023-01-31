2c903bd72116b8bbb062ea21196224dfae6fc7ab
luisodamian <124053103+luisodamian@users.noreply.github.com>
2023-01-31 09:22:38 -0300

    Add files via upload

---
+++ PROYECTO FINAL/cv.js
@@ -0,0 +1,10 @@
+
+document.getElementById("boton").onclick =function(){
+    document.getElementById("demo").innerHTML = "Nacionalidad : Usa Nacimiento: 28/02/1985 Estado Civil : Casado"
+}
+document.getElementById("boton2").onclick =function(){
+    document.getElementById("demo2").innerHTML = "correo electronico: connorjhon@skynet.com"
+}
+document.getElementById("boton4").onclick =function(){
+    document.getElementById("demo4").innerHTML = "John es sistemático, metódico y disciplinado. En su aproximación al trabajo, tiende a seguir muy de cerca las políticas, pautas y reglas. Tiende a seguir el protocolo. Trabaja más eficaz y cómodamente bajo presion."
+}
